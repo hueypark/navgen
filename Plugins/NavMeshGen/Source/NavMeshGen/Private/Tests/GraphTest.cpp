5218a3c4ccbe070c78f6662aa0e419a5d80a6b67
Jaewan Park <jaewan.huey.park@gmail.com>
2022-01-31 13:23:20 +0900

    그래프 자료구조 골격 추가

---
+++ Plugins/NavMeshGen/Source/NavMeshGen/Private/Tests/GraphTest.cpp
@@ -0,0 +1,7 @@
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	GraphTest, "NavMeshGen.Graph", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool GraphTest::RunTest(const FString& Parameters)
+{
+	return true;
+}
